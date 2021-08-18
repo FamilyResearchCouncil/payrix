8603eccf672c36f0754a054e18e1cf59ef944180
Ethan Brace <ethanabrace@gmail.com>
2021-08-18 19:16:26 -0400

    add resources and base url

--- config/payrix.php
+++ config/payrix.php
@@ -1,11 +1,18 @@
 <?php
 
 return [
-    'default_account' => 'default',
+    'base_url' => env('PAYRIX_URL', 'https://api.payrix.com'),
 
+    'default_account' => 'default',
     'accounts'        => [
         'default' => [
             'api-key' => env('PAYRIX_PRIVATE_KEY')
         ]
+    ],
+
+    'resources' => [
+        // override class payrix class mapping here
+        'txns'         => Frc\Payrix\Models\Transaction::class,
+        'transactions' => Frc\Payrix\Models\Transaction::class
     ]
 ];
